02f2fbbe4ba25d03e9ae814cf63da76726bb1311
Pierre Boué <37743737+Macaplix@users.noreply.github.com>
2023-04-21 22:57:50 +0200

    Basic Setup

---
+++ src/test/java/com/macaplix/xmlprettyprint/XmlPrettyPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macaplix.xmlprettyprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlPrettyPrintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
